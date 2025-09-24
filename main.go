6b6c6434df8715ac4a3a1553929c7e4713ddac84
Eric Henrique <ericpf123@gmail.com>
2025-09-24 19:24:37 -0300

    inclusão de PUT DELET e GET

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log/slog"
 	"net/http"
 
@@ -19,7 +20,10 @@ func main() {
 
 	// Routes
 	e.GET("/students", getStudents)
-	e.GET("/teachers", getTeachers)
+	e.POST("/students", createStudent)
+	e.GET("/students/:id", getStudent)
+	e.PUT("/students/:id", updateStudent)
+	e.DELETE("/students/:id", deleteStudent)
 
 	// Start server
 	if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
@@ -32,6 +36,24 @@ func getStudents(c echo.Context) error {
 	return c.String(http.StatusOK, "List of all students ")
 }
 
-func getTeachers(c echo.Context) error {
-	return c.String(http.StatusOK, "List of all teachers ")
+func createStudent(c echo.Context) error {
+	return c.String(http.StatusOK, "Create student ")
+}
+
+func getStudent(c echo.Context) error {
+	id := c.Param("id")
+	getStud := fmt.Sprintf("Get %s student", id)
+	return c.String(http.StatusOK, getStud)
+}
+
+func updateStudent(c echo.Context) error {
+	id := c.Param("id")
+	updateStud := fmt.Sprintf("Update %s student", id)
+	return c.String(http.StatusOK, updateStud)
+}
+
+func deleteStudent(c echo.Context) error {
+	id := c.Param("id")
+	deletStud := fmt.Sprintf("Delete %s student", id)
+	return c.String(http.StatusOK, deletStud)
 }
